145efcaaf8f39f7a00b4a0f402efded23f28330d
d10658 <sergoiwj@duck.com>
2025-11-05 14:19:49 +0200

    integrate Clap for argument parsing and add runtime format detection and conversion functionality

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,76 @@
-use parser_converter::Converter;
-use parser_converter::bin_format::BinRecords;
-use parser_converter::txt_format::TXTRecords;
-use std::fs::File;
-use std::io::BufWriter;
-use std::path::Path;
-use parser_converter::csv_format::CSVRecords;
+use clap::Parser;
+use parser_converter::{
+    Converter, bin_format::BinRecords, csv_format::CSVRecords, txt_format::TXTRecords,
+};
+use std::{
+    fs::File,
+    io::{self, BufWriter},
+    path::PathBuf,
+};
 
-fn main() {
-    let input_path = Path::new("records_example.csv");
-    let output_path = Path::new("res_check_csv.txt");
+#[derive(Parser, Debug)]
+#[command(name = "ypbank_converter")]
+#[command(about = "Converts YPBank transaction records between formats")]
+struct Args {
+    /// Input file path
+    #[arg(long)]
+    input: PathBuf,
 
-    let mut file = File::open(input_path).unwrap();
-    let records = CSVRecords::from_read(&mut file).unwrap();
+    /// Output format: bin, txt, csv
+    #[arg(long)]
+    output_format: String,
+}
 
-    let mut writer = BufWriter::new(File::create(output_path).unwrap());
-    TXTRecords::write_to(records.as_records(), &mut writer).unwrap();
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+    let input_format = detect_format(&args.input).unwrap_or_else(|| "unknown".to_string());
+
+    if input_format == args.output_format {
+        eprintln!("❌ Input and output formats are the same — nothing to convert.");
+        std::process::exit(1);
+    }
+
+    let mut file = File::open(&args.input)?;
+    let mut output_path = PathBuf::new();
+    output_path.push(&args.input);
+    output_path.set_extension(&args.output_format);
+
+    // читаем входной формат и записываем в новый
+    match (input_format.as_str(), args.output_format.as_str()) {
+        ("bin", "txt") => convert::<BinRecords, TXTRecords>(&mut file, &output_path).unwrap(),
+        ("bin", "csv") => convert::<BinRecords, CSVRecords>(&mut file, &output_path).unwrap(),
+
+        ("txt", "bin") => convert::<TXTRecords, BinRecords>(&mut file, &output_path).unwrap(),
+        ("txt", "csv") => convert::<TXTRecords, CSVRecords>(&mut file, &output_path).unwrap(),
+
+        ("csv", "bin") => convert::<CSVRecords, BinRecords>(&mut file, &output_path).unwrap(),
+        ("csv", "txt") => convert::<CSVRecords, TXTRecords>(&mut file, &output_path).unwrap(),
+
+        _ => {
+            eprintln!(
+                "❌ Unknown format combination: {} → {}",
+                input_format, args.output_format
+            );
+            std::process::exit(1);
+        }
+    }
+
+    println!("✅ Конвертация завершена: {:?}", output_path);
+    Ok(())
+}
+
+fn convert<From, To>(reader: &mut File, output_path: &PathBuf) -> Result<(), String>
+where
+    From: Converter,
+    To: Converter,
+{
+    let records = From::from_read(reader)?;
+    let mut writer = BufWriter::new(File::create(output_path).map_err(|e| e.to_string())?);
+    To::write_to(records.as_records(), &mut writer)
+}
+
+fn detect_format(path: &PathBuf) -> Option<String> {
+    path.extension()
+        .and_then(|ext| ext.to_str())
+        .map(|ext| ext.to_lowercase())
 }
